074b7736ee790f9f07c863f332c46a9a062c8ea5
KJoyce43 <kiernan.joyce@vtsd.com>
2016-12-15 08:34:27 -0500

    Make first move
    Always Collude on first move. Also created a test to test this

--- team2.py
+++ team2.py
@@ -17,7 +17,8 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if (my_history=='' and their_history=='' and my_score==0 and their_score==0):
+        return 'b'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
