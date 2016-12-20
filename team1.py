fe00bf68a6a456256fa5e5e5c3dffce1858c110f
KJoyce43 <kiernan.joyce@vtsd.com>
2016-12-20 08:06:43 -0500

    THe actual code
    asdf

--- team1.py
+++ team1.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'HackNSlash' # Only 10 chars displayed.
+strategy_name = 'Passing the class'
+strategy_description = 'Try to win'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,15 +17,26 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if (my_history=='' and their_history=='' and my_score==0 and their_score==0):
+        return 'b'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    # The most recent round is my_history[-1] and their_history[-1]. CHANGED
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
+    if (my_history=="b" and their_history=="c"):
+        return 'b'
+    if (my_history=="bb" and their_history=="cc"):
+        return 'b'
+    if (my_history=="bb" and their_history=="cb"):
+        return 'c'
+    if (my_history=="bbc" and their_history=="cbb"):
+        return 'b'
+    if (my_history=="bbc" and their_history=="cbc"):
+        return 'b'
     return 'c'
 
     
